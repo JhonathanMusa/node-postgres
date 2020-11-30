a7daafa574ca2bcc983c731ca8fc53af1382fc6e
jhonathanmusa <jonathansm891@gmail.com>
2020-11-29 20:11:24 -0500

    second commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000
 
 // middlewares
 app.use(express.json());
@@ -8,5 +9,5 @@ app.use(express.urlencoded({ extended: false }));
 // routes
 app.use(require("./routes/index"));
 
-app.listen(8080);
-console.log("Server on port 8080");
+app.listen(PORT);
+console.log(`Server on port ${PORT}`);
